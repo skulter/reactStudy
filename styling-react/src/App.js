c77582d6ec3b77dce0892e0316636c178e7d5f81
jinho <skukter@gmail.com>
2021-07-16 15:56:54 +0900

    yarn eject 전의 커밋

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import SassComponent from './SassComponent.';
+import React, { Component } from 'react';
+import StyledComponent from './StyledComponent';
 
-
-function App() {
-  return (
-    <>
-    <SassComponent/>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <StyledComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
